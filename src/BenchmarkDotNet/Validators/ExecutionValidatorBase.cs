636e406348f2822e3baf51356f340729acc26de3
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-09-28 02:24:59 +0300

    Try Tim's AwaitHelper

--- src/BenchmarkDotNet/Validators/ExecutionValidatorBase.cs
+++ src/BenchmarkDotNet/Validators/ExecutionValidatorBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Extensions;
@@ -155,25 +156,16 @@ namespace BenchmarkDotNet.Validators
                 return false;
             }
 
-            var returnType = task.GetType();
-            if (returnType.IsGenericType && returnType.GetGenericTypeDefinition() == typeof(ValueTask<>))
+            var getResultMethod = AwaitHelper.GetGetResultMethod(result.GetType());
+
+            if (getResultMethod is null)
             {
-                var asTaskMethod = task.GetType().GetMethod("AsTask");
-                task = asTaskMethod.Invoke(task, null);
+                return false;
             }
 
-            if (task is ValueTask valueTask)
-                task = valueTask.AsTask();
+            result = getResultMethod.Invoke(null, new[] { task });
 
-            if (task is Task t)
-            {
-                if (TryGetTaskResult(t, out var taskResult))
-                    result = taskResult;
-
-                return true;
-            }
-
-            return false;
+            return getResultMethod.ReturnType != typeof(void);
         }
 
 // https://stackoverflow.com/a/52500763
@@ -292,6 +284,123 @@ namespace BenchmarkDotNet.Validators
 
                 return result;
             }
+        }
+    }
+
+    public static class AwaitHelper
+    {
+        private class ValueTaskWaiter
+        {
+            // We use thread static field so that multiple threads can use individual lock object and callback.
+            [ThreadStatic]
+            private static ValueTaskWaiter ts_current;
+            internal static ValueTaskWaiter Current => ts_current ??= new ValueTaskWaiter();
+
+            private readonly Action awaiterCallback;
+            private bool awaiterCompleted;
+
+            private ValueTaskWaiter()
+            {
+                awaiterCallback = AwaiterCallback;
+            }
+
+            private void AwaiterCallback()
+            {
+                lock (this)
+                {
+                    awaiterCompleted = true;
+                    System.Threading.Monitor.Pulse(this);
+                }
+            }
+
+            // Hook up a callback instead of converting to Task to prevent extra allocations on each benchmark run.
+            internal void Wait(ConfiguredValueTaskAwaitable.ConfiguredValueTaskAwaiter awaiter)
+            {
+                lock (this)
+                {
+                    awaiterCompleted = false;
+                    awaiter.UnsafeOnCompleted(awaiterCallback);
+                    // Check if the callback executed synchronously before blocking.
+                    if (!awaiterCompleted)
+                    {
+                        System.Threading.Monitor.Wait(this);
+                    }
+                }
+            }
+
+            internal void Wait<T>(ConfiguredValueTaskAwaitable<T>.ConfiguredValueTaskAwaiter awaiter)
+            {
+                lock (this)
+                {
+                    awaiterCompleted = false;
+                    awaiter.UnsafeOnCompleted(awaiterCallback);
+                    // Check if the callback executed synchronously before blocking.
+                    if (!awaiterCompleted)
+                    {
+                        System.Threading.Monitor.Wait(this);
+                    }
+                }
+            }
+        }
+
+        // we use GetAwaiter().GetResult() because it's fastest way to obtain the result in blocking way,
+        // and will eventually throw actual exception, not aggregated one
+        public static void GetResult(Task task) => task.GetAwaiter().GetResult();
+
+        public static T GetResult<T>(Task<T> task) => task.GetAwaiter().GetResult();
+
+        // ValueTask can be backed by an IValueTaskSource that only supports asynchronous awaits, so we have to hook up a callback instead of calling .GetAwaiter().GetResult() like we do for Task.
+        // The alternative is to convert it to Task using .AsTask(), but that causes allocations which we must avoid for memory diagnoser.
+        public static void GetResult(ValueTask task)
+        {
+            // Don't continue on the captured context, as that may result in a deadlock if the user runs this in-process.
+            var awaiter = task.ConfigureAwait(false).GetAwaiter();
+            if (!awaiter.IsCompleted)
+            {
+                ValueTaskWaiter.Current.Wait(awaiter);
+            }
+            awaiter.GetResult();
+        }
+
+        public static T GetResult<T>(ValueTask<T> task)
+        {
+            // Don't continue on the captured context, as that may result in a deadlock if the user runs this in-process.
+            var awaiter = task.ConfigureAwait(false).GetAwaiter();
+            if (!awaiter.IsCompleted)
+            {
+                ValueTaskWaiter.Current.Wait(awaiter);
+            }
+            return awaiter.GetResult();
+        }
+
+        internal static MethodInfo GetGetResultMethod(Type taskType)
+        {
+            if (!taskType.IsGenericType)
+            {
+                return typeof(AwaitHelper).GetMethod(nameof(AwaitHelper.GetResult), BindingFlags.Public | BindingFlags.Static, null, new Type[1] { taskType }, null);
+            }
+
+            Type compareType = taskType.GetGenericTypeDefinition() == typeof(ValueTask<>) ? typeof(ValueTask<>)
+                : typeof(Task).IsAssignableFrom(taskType.GetGenericTypeDefinition()) ? typeof(Task<>)
+                : null;
+            if (compareType == null)
+            {
+                return null;
+            }
+            var resultType = taskType
+                .GetMethod(nameof(Task.GetAwaiter), BindingFlags.Public | BindingFlags.Instance)
+                .ReturnType
+                .GetMethod(nameof(TaskAwaiter.GetResult), BindingFlags.Public | BindingFlags.Instance)
+                .ReturnType;
+            return typeof(AwaitHelper).GetMethods(BindingFlags.Public | BindingFlags.Static)
+                .First(m =>
+                {
+                    if (m.Name != nameof(AwaitHelper.GetResult)) return false;
+                    Type paramType = m.GetParameters().First().ParameterType;
+                    // We have to compare the types indirectly, == check doesn't work.
+                    return paramType.Assembly == compareType.Assembly && paramType.Namespace == compareType.Namespace && paramType.Name == compareType.Name;
+                })
+                .MakeGenericMethod(new[] { resultType });
         }
     }
 }
