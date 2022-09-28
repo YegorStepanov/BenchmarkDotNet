03b65c52d6189311166734796924ce3ff440a6c2
Yegor Stepanov <yegor.stepanov@outlook.com>
2022-09-28 16:09:06 +0300

    Disable xUnit parallelism (tests passed)

--- tests/BenchmarkDotNet.Tests/Validators/ExecutionValidatorTests.cs
+++ tests/BenchmarkDotNet.Tests/Validators/ExecutionValidatorTests.cs
@@ -10,6 +10,7 @@ using Xunit.Abstractions;
 
 namespace BenchmarkDotNet.Tests.Validators
 {
+    [Collection("Disable parallelism")]
     public class ExecutionValidatorTests
     {
         private readonly ITestOutputHelper testOutputHelper;
--- tests/BenchmarkDotNet.Tests/Validators/ReturnValueValidatorTests.cs
+++ tests/BenchmarkDotNet.Tests/Validators/ReturnValueValidatorTests.cs
@@ -10,6 +10,7 @@ using Xunit.Abstractions;
 
 namespace BenchmarkDotNet.Tests.Validators
 {
+    [Collection("Disable parallelism")]
     public class ReturnValueValidatorTests
     {
         private readonly ITestOutputHelper testOutputHelper;
